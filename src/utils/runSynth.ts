a193ac57f3f095f66612650300758aa588404e01
Henry <henry@kirkness.com>
2021-02-11 11:29:55 +0000

    fix: clean out manifest before synthesizing again

--- src/utils/runSynth.ts
+++ src/utils/runSynth.ts
@@ -1,11 +1,15 @@
 import execa from 'execa';
 import {twisters} from './twisters';
+import {writeManifest} from './writeManifest';
 
 export const runSynth = async (options: {
   context: string[];
   profile?: string;
   app?: string;
 }): Promise<void> => {
+  // Create a fresh manifest
+  writeManifest({region: '', lambdas: {}});
+
   const synthProgressText = 'synthesizing CDK app';
   twisters.put('synth', {text: synthProgressText});
   await execa(
