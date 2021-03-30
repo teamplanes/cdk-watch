5135c815a7c0635b87def80dcc6b88db79bb6fa2
Henry <henry@kirkness.com>
2021-03-30 10:14:45 +0100

    fix: add ensureDir to manifest write

--- src/lib/writeManifest.ts
+++ src/lib/writeManifest.ts
@@ -4,10 +4,8 @@ import {CdkWatchManifest} from '../types.d';
 import {CDK_WATCH_MANIFEST_FILE_NAME} from '../consts';
 
 export const writeManifest = (manifest: CdkWatchManifest): void => {
-  const manifestPath = path.join(
-    process.cwd(),
-    'cdk.out',
-    CDK_WATCH_MANIFEST_FILE_NAME,
-  );
+  const cdkOut = path.join(process.cwd(), 'cdk.out');
+  const manifestPath = path.join(cdkOut, CDK_WATCH_MANIFEST_FILE_NAME);
+  fs.ensureDirSync(cdkOut);
   fs.writeJsonSync(manifestPath, manifest);
 };
