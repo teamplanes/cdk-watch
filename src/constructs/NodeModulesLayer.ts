ac877ea66f88f01b81f022d86613e7e4e08b649e
Hennerz <henry@kirkness.com>
2022-03-29 21:32:32 +0100

    fix: remove import from cdk lib

--- src/constructs/NodeModulesLayer.ts
+++ src/constructs/NodeModulesLayer.ts
@@ -1,7 +1,3 @@
-import {
-  extractDependencies,
-  findUp,
-} from 'aws-cdk-lib/aws-lambda-nodejs/lib/util';
 import execa from 'execa';
 import * as fs from 'fs-extra';
 import * as path from 'path';
@@ -27,6 +23,128 @@ enum Installer {
 enum LockFile {
   NPM = 'package-lock.json',
   YARN = 'yarn.lock',
+}
+
+/**
+ * Find the lowest of multiple files by walking up parent directories. If
+ * multiple files exist at the same level, they will all be returned.
+ * Ref: https://github.com/aws/aws-cdk/blob/master/packages/@aws-cdk/aws-lambda-nodejs/lib/util.ts
+ */
+export function findUpMultiple(
+  names: string[],
+  directory: string = process.cwd(),
+): string[] {
+  const absoluteDirectory = path.resolve(directory);
+
+  const files = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const name of names) {
+    const file = path.join(directory, name);
+    if (fs.existsSync(file)) {
+      files.push(file);
+    }
+  }
+
+  if (files.length > 0) {
+    return files;
+  }
+
+  const {root} = path.parse(absoluteDirectory);
+  if (absoluteDirectory === root) {
+    return [];
+  }
+
+  return findUpMultiple(names, path.dirname(absoluteDirectory));
+}
+
+/**
+ * Find a file by walking up parent directories
+ * Ref: https://github.com/aws/aws-cdk/blob/master/packages/@aws-cdk/aws-lambda-nodejs/lib/util.ts
+ */
+export function findUp(
+  name: string,
+  directory: string = process.cwd(),
+): string | undefined {
+  return findUpMultiple([name], directory)[0];
+}
+
+/**
+ * Ref: https://github.com/aws/aws-cdk/blob/master/packages/@aws-cdk/aws-lambda-nodejs/lib/util.ts
+ */
+export function tryGetModuleVersionFromPkg(
+  mod: string,
+  pkgJson: {[key: string]: any},
+  pkgPath: string,
+): string | undefined {
+  const dependencies = {
+    ...(pkgJson.dependencies ?? {}),
+    ...(pkgJson.devDependencies ?? {}),
+    ...(pkgJson.peerDependencies ?? {}),
+  };
+
+  if (!dependencies[mod]) {
+    return undefined;
+  }
+
+  // If it's a "file:" version, make it absolute
+  const fileMatch = dependencies[mod].match(/file:(.+)/);
+  if (fileMatch && !path.isAbsolute(fileMatch[1])) {
+    const absoluteFilePath = path.join(path.dirname(pkgPath), fileMatch[1]);
+    return `file:${absoluteFilePath}`;
+  }
+
+  return dependencies[mod];
+}
+
+/**
+ * Returns a module version by requiring its package.json file
+ *
+ * Ref: https://github.com/aws/aws-cdk/blob/master/packages/@aws-cdk/aws-lambda-nodejs/lib/util.ts
+ */
+export function tryGetModuleVersionFromRequire(
+  mod: string,
+): string | undefined {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    // eslint-disable-next-line import/no-dynamic-require, global-require, @typescript-eslint/no-var-requires
+    return require(`${mod}/package.json`).version;
+  } catch (err) {
+    return undefined;
+  }
+}
+
+/**
+ * Extract versions for a list of modules.
+ *
+ * First lookup the version in the package.json and then fallback to requiring
+ * the module's package.json. The fallback is needed for transitive dependencies.
+ *
+ * Ref: https://github.com/aws/aws-cdk/blob/master/packages/@aws-cdk/aws-lambda-nodejs/lib/util.ts
+ */
+export function extractDependencies(
+  pkgPath: string,
+  modules: string[],
+): {[key: string]: string} {
+  const dependencies: {[key: string]: string} = {};
+
+  // Use require for cache
+  // eslint-disable-next-line import/no-dynamic-require, global-require, @typescript-eslint/no-var-requires
+  const pkgJson = require(pkgPath);
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const mod of modules) {
+    const version =
+      tryGetModuleVersionFromPkg(mod, pkgJson, pkgPath) ??
+      tryGetModuleVersionFromRequire(mod);
+    if (!version) {
+      throw new Error(
+        `Cannot extract version for module '${mod}'. Check that it's referenced in your package.json or installed.`,
+      );
+    }
+    dependencies[mod] = version;
+  }
+
+  return dependencies;
 }
 
 /**
